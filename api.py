b5c8bc2d3eecc05f834543b4d990bd74c4e919a1
Sukhman Brar <sukhmanb96@gmail.com>
2019-12-16 00:02:48 -0800

    Write function to get team keys of the teams that the current logged in user manages. These can be used to retrieve the current rosters for each team.

--- api.py
+++ api.py
@@ -47,10 +47,50 @@ def getleaguesandteams():
 	user=MongoClient(app.config["DATABASE_URI"]).lineup2date.users.find_one({"email":email})
 	oauth2=Yahoo_OAuth2(email,user["consumer_key"],user["consumer_secret"],user["redirect_uri"],False,None)
 	thebaseurl='https://fantasysports.yahooapis.com/fantasy/v2/'
-	theurl=thebaseurl+'users;use_login=1/games/teams' #returns all my teams
-	response=oauth2.session.get(theurl, params={"format": "json"})
-	print(response.json())
+	nflurl=thebaseurl+'users;use_login=1/games;game_keys=nfl/teams' #returns my 2019 nfl team information
+	nbaurl=thebaseurl+'users;use_login=1/games;game_keys=nba/teams' #returns my 2019 nba teams information
+	nhlurl=thebaseurl+'users;use_login=1/games;game_keys=nhl/teams' #returns my 2019 nba teams information
+	nflresponse=oauth2.session.get(nflurl, params={"format": "json"})
+	nbaresponse=oauth2.session.get(nbaurl, params={"format": "json"})
+	nhlresponse=oauth2.session.get(nhlurl, params={"format": "json"})
 
+	nflseasondictionary=nflresponse.json()
+	nbaseasondictionary=nbaresponse.json()
+	nhlseasondictionary=nhlresponse.json()
+
+
+	numnflteams=len(nflseasondictionary["fantasy_content"]["users"]["0"]["user"][1]["games"]["0"]["game"][1]["teams"])-1
+	numnbateams=len(nbaseasondictionary["fantasy_content"]["users"]["0"]["user"][1]["games"]["0"]["game"][1]["teams"])-1
+	numnhlteams=len(nhlseasondictionary["fantasy_content"]["users"]["0"]["user"][1]["games"]["0"]["game"][1]["teams"])-1
+	nflleagueids=[]
+	nbaleagueids=[]
+	nhlleagueids=[]
+
+	print("NFL:")
+	for i in range(numnflteams):
+		thenflteam=nflseasondictionary["fantasy_content"]["users"]["0"]["user"][1]["games"]["0"]["game"][1]["teams"][str(i)]
+		print(thenflteam)
+		print("\n")
+		nflleagueids.append(thenflteam["team"][0][0]["team_key"])
+
+	print("NBA:")
+	for i in range(numnbateams):
+		thenbateam=nbaseasondictionary["fantasy_content"]["users"]["0"]["user"][1]["games"]["0"]["game"][1]["teams"][str(i)]
+		print(thenbateam)
+		print("\n")
+		nbaleagueids.append(thenbateam["team"][0][0]["team_key"])
+
+    # 0 NHL TEAMS IN THIS USERS CASE
+	# print("NHL:")
+	# for i in range(numnhlteams):
+	# 	thenhlteam=nhlseasondictionary["fantasy_content"]["users"]["0"]["user"][1]["games"]["0"]["game"][1]["teams"][str(i)]
+	# 	print(thenhlteam)
+	# 	print("\n")
+	# 	nhlleagueids.append(thenhlteam["team"][0][0]["team_key"])
+	print(nflleagueids) #contains the NFL team keys of the teams that the current logged in user manages
+	print(nbaleagueids) #contains the NBA team keys of the teams that the current logged in user manages
+	# print(nhlleagueids) #contains the NHL team keys of the teams that the current logged in user manages
+    # These team keys can be used to retrieve the roster of the NFL and NBA teams the user manages
 
 	# Then oauth2=Yahoo_OAuth2(app.config["CONSUMER_KEY"],app.config["CONSUMER_SECRET"],app.config["REDIRECT_URI"])
 	# IN Yahoo_OAuth2, go through the procedure as if its the first time (unless access token, refresh token, etc exist) in which case you look into refreshing
